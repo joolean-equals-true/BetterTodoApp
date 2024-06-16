5f491f315464dccf179ed56900e30db396f3ed1d
Lance Skinner <sirlancelot2604@outlook.com>
2024-06-16 14:15:51 -0400

    Change Styling, add more components

---
+++ FrontEnd/src/app/components/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() text?: String;
+  @Input() backgroundColor?: String;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
